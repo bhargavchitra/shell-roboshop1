c10758867cc86749c24499b5aeab8e07e2d87526
bhargavchitra <bhargavchitra05.com>
2026-07-29 14:33:51 +0530

    action

---
+++ mysql.sh
@@ -0,0 +1,42 @@
+#!/bin/bash 
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-roboshop"
+LOGS_FILE="$LOGS_FOLDER/$0.log"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+B="\e[34m"
+M="\e[35m"
+C="\e[36m"
+W="\e[37m"
+N="\e[0m"
+SCRIPT_DIR=$PWD
+MONGODB_HOST=mysql.bunnyone.online
+
+if [ $USERID -ne 0 ]; then
+    echo -e "$R Please run this script as root user access $N" | tee -a $LOGS_FILE
+    exit 1
+fi
+
+mkdir -p $LOGS_FOLDER
+
+VALIDATE(){
+    if [ $? -ne 0 ]; then
+        echo -e "$2  ... $M FAILURE $N" | tee -a $LOGS_FILE
+        exit 1
+  else
+       echo -e "$2   ... $G success $N" | tee -a $LOGS_FILE
+ fi
+}
+
+dnf install mysql-server -y
+VALIDATE $? "install mysql-server"
+
+systemctl enable mysqld
+systemctl start mysqld  
+VALIDATE $? "Enable and start mysql"
+
+# get the password from user 
+mysql_secure_installation --set-root-pass RoboShop@1
+VALIDATE $? "setup root password"
