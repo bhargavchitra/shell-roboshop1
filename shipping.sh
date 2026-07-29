b4b15b4dfb14447a32217b98fc402b7233ae3a1c
bhargavchitra <bhargavchitra05.com>
2026-07-29 15:01:32 +0530

    action

---
+++ shipping.sh
@@ -0,0 +1,81 @@
+#!/bin/bash 
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-roboshop"
+LOGS_FILE="$LOGS_FOLDER/$0.log"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+B="\e[34m"
+M="\e[35m"
+C="\e[36m"
+W="\e[37m"
+N="\e[0m"
+SCRIPT_DIR=$PWD
+MYSQL_HOST=mysql.bunnyone.online
+
+if [ $USERID -ne 0 ]; then
+    echo -e "$R Please run this script as root user access $N" | tee -a $LOGS_FILE
+    exit 1
+fi
+
+mkdir -p $LOGS_FOLDER
+
+VALIDATE(){
+    if [ $? -ne 0 ]; then
+        echo -e "$2  ... $M FAILURE $N" | tee -a $LOGS_FILE
+        exit 1
+  else
+       echo -e "$2   ... $G success $N" | tee -a $LOGS_FILE
+ fi
+}
+
+dnf install maven -y &>>$LOGS_FILE
+VALIDATE $? "Install maven"
+
+id roboshop &>>$LOGS_FILE
+if [ $? -ne 0 ]; then 
+useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop
+else 
+    echo -e "Roboshop user already exist... $Y SKIPPING $N"
+fi 
+
+mkdir -p /app 
+VALIDATE "creating app directory"
+
+
+curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping-v3.zip 
+VALIDATE $? "Downloading shipping code"
+
+cd /app 
+VALIDATE $? "Moving to app directory"
+
+rm -rf /app/*
+VALIDATE $? "Removing existing code" 
+
+unzip /tmp/shipping.zip
+VALIDATE $? " Uzip catalogue code "
+
+
+cd /app 
+mvn clean package &>>$LOGS_FILE
+VALIDATE $? "Installing and building shipping"
+
+mv target/shipping-1.0.jar shipping.jar 
+VALIDATE $? "Installing and Renaming shipping"
+
+cp $SCRIPT_DIR/shipping.service /etc/systemd/system/shipping.service
+VALIDATE $? "Created systemctl services"
+
+dnf install mysql -y 
+VALIDATE $? "Installing MYSQL"
+
+mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/schema.sql
+mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/app-user.sql 
+mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/master-data.sql
+
+systemctl restart shipping
+systemctl enable shipping 
+systemctl start shipping
+VALIDATE $? "Starting and enabling catalogue"
+
