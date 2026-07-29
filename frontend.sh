e57f1b6528459ae640e9b82fd1d17687bba02bd5
bhargavchitra <bhargavchitra05.com>
2026-07-29 16:26:13 +0530

    action

---
+++ frontend.sh
@@ -0,0 +1,68 @@
+
+#!/bin/bash 
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-roboshop"
+LOGS_FILE="$LOGS_FOLDER/$0.log"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+B="\e[34m"
+M="\e[35m"
+C="\e[36m"
+W="\e[37m"
+N="\e[0m"
+SCRIPT_DIR=$PWD
+
+
+if [ $USERID -ne 0 ]; then
+    echo -e "$R Please run this script as root user access $N" | tee -a $LOGS_FILE
+    exit 1
+fi
+
+mkdir -p $LOGS_FOLDER
+
+VALIDATE(){
+    if [ $? -ne 0 ]; then
+        echo -e "$2  ... $M FAILURE $N" | tee -a $LOGS_FILE
+        exit 1
+  else
+       echo -e "$2   ... $G success $N" | tee -a $LOGS_FILE
+ fi
+}
+
+dnf module disable nginx -y &>>$LOGS_FILE
+dnf module enable nginx:1.24 -y &>>$LOGS_FILE
+dnf install nginx -y &>>$LOGS_FILE
+VALIDATE "installing nginx"
+
+systemctl enable nginx &>>$LOGS_FILE
+systemctl start nginx
+VALIDATE "Enabled and started"
+
+rm -rf /usr/share/nginx/html/* 
+VALIDATE $? "remove default content"
+
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip
+cd /usr/share/nginx/html 
+unzip /tmp/frontend.zip
+VALIDATE $? "Downloaded and unzippe frontend"
+
+rm -rf /etc/nginx/nginx.conf
+
+cp $script_dir/nginx.conf /etc/nginx/nginx.conf
+VALIDATE $? "Copied our nginx xonf file"
+
+systemctl restart nginx 
+VALIDATE $? " restart"
+
+
+
+
+
+
+
+
+
+
+
