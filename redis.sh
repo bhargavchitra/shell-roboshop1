60a6efb8b23dd2fd58846f21d6f58563db6a32f0
bhargavchitra <bhargavchitra05.com>
2026-07-29 12:50:27 +0530

    action

--- redis.sh
+++ redis.sh
@@ -30,15 +30,13 @@ VALIDATE(){
 
 
 dnf module disable redis -y &>>$LOGS_FILE
-VALIDATE $? "Disable redis Default version"
-
 dnf module enable redis:7 -y &>>$LOGS_FILE
-VALIDATE $? "Enable required module"
+VALIDATE $? "Enable redis:7"
 
 dnf install redis -y &>>$LOGS_FILE
 VALIDATE $? "installing redis"
 
-sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected -mode no' /etc/redis/redis.conf
+sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
 VALIDATE $? "Allowing remote connections" 
 
 systemctl enable redis &>>$LOGS_FILE
