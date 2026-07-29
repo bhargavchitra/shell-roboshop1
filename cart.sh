374ac84b322eef6894245143b02aa4b45e621262
bhargavchitra <bhargavchitra05.com>
2026-07-29 14:19:05 +0530

    action

--- cart.sh
+++ cart.sh
@@ -12,7 +12,7 @@ C="\e[36m"
 W="\e[37m"
 N="\e[0m"
 SCRIPT_DIR=$PWD
-MONGODB_HOST=cart.bunnyone.online
+MONGODB_HOST=mongodb.bunnyone.online
 
 if [ $USERID -ne 0 ]; then
     echo -e "$R Please run this script as root user access $N" | tee -a $LOGS_FILE
@@ -30,18 +30,18 @@ VALIDATE(){
  fi
 }
 
-dnf module disable nodejs -y
+dnf module disable nodejs -y  &>>$LOGS_FILE
 VALIDATE $? "Disable Nodejs Default version"
 
-dnf module enable nodejs:20 -y
+dnf module enable nodejs:20 -y  &>>$LOGS_FILE
 VALIDATE $? "Enable required module"
 
-dnf install nodejs -y
+dnf install nodejs -y  &>>$LOGS_FILE
 VALIDATE $? "Install NodeJS"
 
 id roboshop &>>$LOGS_FILE
 if [ $? -ne 0 ]; then 
-useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop
+useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop  &>>$LOGS_FILE
 else 
     echo -e "Roboshop cart already exist... $Y SKIPPING $N"
 fi 
@@ -49,7 +49,7 @@ fi
 mkdir /app 
 VALIDATE "creating app directory"
 
-curl -L -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart-v3.zip
+curl -L -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart-v3.zip  &>>$LOGS_FILE
 VALIDATE $? "Downloading cart code"
 
 cd /app 
@@ -58,16 +58,16 @@ VALIDATE $? "Moving to app directory"
 rm -rf /app/*
 VALIDATE $? "Removing existing code" 
 
-unzip /tmp/cart.zip
+unzip /tmp/cart.zip  &>>$LOGS_FILE
 VALIDATE $? " Uzip cart code "
 
-npm install 
+npm install  &>>$LOGS_FILE
 VALIDATE $? "Installing dependencies." 
 
 cp $SCRIPT_DIR/cart.service /etc/systemd/system/cart.service
 VALIDATE $? "Created systemctl services"
 
 systemctl daemon-reload
-systemctl enable cart 
+systemctl enable cart &>>$LOGS_FILE
 systemctl start cart
 VALIDATE $? "Starting and enabling cart"
